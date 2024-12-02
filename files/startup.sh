5b5b65792c5e4063aa92e071ae0e0b502d2985b3
Tyler North <me@tyler-north.com>
2024-12-01 16:23:58 -0800

    add new module, oci cli setups (#4)

--- files/startup.sh
+++ files/startup.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+if [ -f "/opt/mirror/env/custom-startup.sh" ]; then
+    chmod +x /opt/mirror/env/custom-startup.sh
+    bash /opt/mirror/env/custom-startup.sh
+fi
+
 cp /opt/mirror/env/config.js /opt/mirror/MagicMirror/config/
 cd /opt/mirror/MagicMirror/
 npm run server
